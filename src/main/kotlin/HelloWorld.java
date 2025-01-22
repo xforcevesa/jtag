cdc5864d0f932082f279e86a7374e21505d22c0d
xforcevesa <nomodeset@qq.com>
2025-01-22 18:01:40 +0800

    update dump

--- src/main/kotlin/HelloWorld.java
+++ src/main/kotlin/HelloWorld.java
@@ -1,4 +1,13 @@
-package org.jtag;
-
+// For ASM Analysis
 public class HelloWorld {
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello: " + fibonacci(10));
+    }
 }
--- src/main/kotlin/dump.kt
+++ src/main/kotlin/dump.kt
@@ -1,17 +1,12 @@
 package org.jtag
-
-import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes
-import org.objectweb.asm.ClassWriter
-import org.objectweb.asm.Label
+import org.objectweb.asm.*
 
 object HelloWorldDump {
-    // Dump a HelloWorld Java bytecode
     @Throws(Exception::class)
     fun dump(): ByteArray {
-        val classWriter = ClassWriter(0)
-        var methodVisitor: MethodVisitor
+        val classWriter = ClassWriter(ClassWriter.COMPUTE_FRAMES or ClassWriter.COMPUTE_MAXS)
 
+        // public class HelloWorld
         classWriter.visit(
             Opcodes.V11,
             Opcodes.ACC_PUBLIC or Opcodes.ACC_SUPER,
@@ -21,25 +16,79 @@ object HelloWorldDump {
             null
         )
 
+        // Source file
         classWriter.visitSource("HelloWorld.java", null)
 
+        classWriter.visitInnerClass(
+            "java/lang/invoke/MethodHandles\$Lookup",
+            "java/lang/invoke/MethodHandles",
+            "Lookup",
+            Opcodes.ACC_PUBLIC or Opcodes.ACC_FINAL or Opcodes.ACC_STATIC
+        )
+
         run {
-            methodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null)
+            // public HelloWorld()
+            val methodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null)
             methodVisitor.visitCode()
             val label0 = Label()
             methodVisitor.visitLabel(label0)
             methodVisitor.visitLineNumber(1, label0)
+            // return Object()
             methodVisitor.visitVarInsn(Opcodes.ALOAD, 0)
             methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false)
             methodVisitor.visitInsn(Opcodes.RETURN)
             val label1 = Label()
             methodVisitor.visitLabel(label1)
+            // this = HelloWorld()
             methodVisitor.visitLocalVariable("this", "LHelloWorld;", null, label0, label1, 0)
-            methodVisitor.visitMaxs(1, 1)
+            // MAX STACK = 1
+            // MAX LOCALS = 1
+            methodVisitor.visitMaxs(0, 0)
             methodVisitor.visitEnd()
         }
         run {
-            methodVisitor = classWriter.visitMethod(
+            // private static fibonacci(int a)
+            val methodVisitor =
+                classWriter.visitMethod(Opcodes.ACC_PRIVATE or Opcodes.ACC_STATIC, "fibonacci", "(I)I", null, null)
+            methodVisitor.visitCode()
+            val label0 = Label()
+            methodVisitor.visitLabel(label0)
+            methodVisitor.visitLineNumber(3, label0)
+            // if (a <= 1)
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 0)
+            methodVisitor.visitInsn(Opcodes.ICONST_1)
+            val label1 = Label()
+            methodVisitor.visitJumpInsn(Opcodes.IF_ICMPGT, label1)
+            val label2 = Label()
+            methodVisitor.visitLabel(label2)
+            methodVisitor.visitLineNumber(4, label2)
+            // return a;
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 0)
+            methodVisitor.visitInsn(Opcodes.IRETURN)
+            methodVisitor.visitLabel(label1)
+            methodVisitor.visitLineNumber(6, label1)
+            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null)
+            // else
+            // return fibonacci(a - 1) + fibonacci(a - 2);
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 0)
+            methodVisitor.visitInsn(Opcodes.ICONST_1)
+            methodVisitor.visitInsn(Opcodes.ISUB)
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "HelloWorld", "fibonacci", "(I)I", false)
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 0)
+            methodVisitor.visitInsn(Opcodes.ICONST_2)
+            methodVisitor.visitInsn(Opcodes.ISUB)
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "HelloWorld", "fibonacci", "(I)I", false)
+            methodVisitor.visitInsn(Opcodes.IADD)
+            methodVisitor.visitInsn(Opcodes.IRETURN)
+            val label3 = Label()
+            methodVisitor.visitLabel(label3)
+            methodVisitor.visitLocalVariable("n", "I", null, label0, label3, 0)
+            methodVisitor.visitMaxs(0, 0)
+            methodVisitor.visitEnd()
+        }
+        run {
+            // public static void main(String[] args)
+            val methodVisitor = classWriter.visitMethod(
                 Opcodes.ACC_PUBLIC or Opcodes.ACC_STATIC,
                 "main",
                 "([Ljava/lang/String;)V",
@@ -49,9 +98,23 @@ object HelloWorldDump {
             methodVisitor.visitCode()
             val label0 = Label()
             methodVisitor.visitLabel(label0)
-            methodVisitor.visitLineNumber(3, label0)
+            methodVisitor.visitLineNumber(10, label0)
+            // System.out.println(fibonacci(12))
             methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
-            methodVisitor.visitLdcInsn("Hello World")
+            methodVisitor.visitIntInsn(Opcodes.BIPUSH, 12)
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESTATIC, "HelloWorld", "fibonacci", "(I)I", false)
+            methodVisitor.visitInvokeDynamicInsn(
+                "makeConcatWithConstants",
+                "(I)Ljava/lang/String;",
+                Handle(
+                    Opcodes.H_INVOKESTATIC,
+                    "java/lang/invoke/StringConcatFactory",
+                    "makeConcatWithConstants",
+                    "(Ljava/lang/invoke/MethodHandles\$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/invoke/CallSite;",
+                    false
+                ),
+                *arrayOf<Any>("Hello: \u0001")
+            )
             methodVisitor.visitMethodInsn(
                 Opcodes.INVOKEVIRTUAL,
                 "java/io/PrintStream",
@@ -61,12 +124,12 @@ object HelloWorldDump {
             )
             val label1 = Label()
             methodVisitor.visitLabel(label1)
-            methodVisitor.visitLineNumber(4, label1)
+            methodVisitor.visitLineNumber(11, label1)
             methodVisitor.visitInsn(Opcodes.RETURN)
             val label2 = Label()
             methodVisitor.visitLabel(label2)
             methodVisitor.visitLocalVariable("args", "[Ljava/lang/String;", null, label0, label2, 0)
-            methodVisitor.visitMaxs(2, 1)
+            methodVisitor.visitMaxs(0, 0)
             methodVisitor.visitEnd()
         }
         classWriter.visitEnd()
@@ -74,5 +137,3 @@ object HelloWorldDump {
         return classWriter.toByteArray()
     }
 }
-
-
